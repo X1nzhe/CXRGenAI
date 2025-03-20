6c5abf67b4e4605c5858ac045ee6c1670695f69c
Xinzhe <38831183+P1Za@users.noreply.github.com>
2025-03-20 15:53:00 -0700

    bug-fixes

--- models/stable_diffusion_generator.py
+++ models/stable_diffusion_generator.py
@@ -44,7 +44,7 @@ class XRayGenerator:
             r=8,
             lora_alpha=16,
             lora_dropout=0.1,
-            target_modules=["cross_attn"]
+            target_modules=["to_k", "to_q", "to_v", "to_out.0"]
         )
         self.pipeline.unet = get_peft_model(self.pipeline.unet, self.lora_config)
     #
