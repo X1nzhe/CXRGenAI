79ea0b185288df2db3f1c2961103dff2dc45b94b
Xinzhe <38831183+P1Za@users.noreply.github.com>
2025-03-31 15:01:27 -0700

    updates

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ LEARNING_RATE = 1e-4
 # Product config
 EPOCHS = 100
 K_FOLDS = 5
-IMAGE_WIDTH = 360
-IMAGE_HEIGHT = 360
+IMAGE_WIDTH = 256
+IMAGE_HEIGHT = 256
 NUM_INFERENCE_STEPS = 500
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
