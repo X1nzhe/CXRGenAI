c6ec778982711942e27fc5fe49c7208bb664bbb1
Xinzhe <38831183+P1Za@users.noreply.github.com>
2025-05-06 12:47:07 -0700

    update

--- main.py
+++ main.py
@@ -32,9 +32,11 @@ def main():
     print(f"\nRunning with environment: {config.ENV}")
 
     if args.mode == "train":
-        print("\n Hyperparameters searching...")
-        best_params = run_hpo(n_trials=30)
-        batch_size = best_params["batch_size"]
+        batch_size = config.BATCH_SIZE
+        if args.hpo:
+            print("\n Hyperparameters searching...")
+            best_params = run_hpo(n_trials=30)
+            batch_size = best_params["batch_size"]
 
         print("\nStart model training...")
         print(f"Epochs: {config.EPOCHS}, K_folds: {config.K_FOLDS}, Batch size: {batch_size}, Image width: {config.IMAGE_WIDTH}, Image height: {config.IMAGE_HEIGHT}, Number of inference steps: {config.NUM_INFERENCE_STEPS}")
