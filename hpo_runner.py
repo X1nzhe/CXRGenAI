f68d723c0d792aa890dba44dc8796208a89e270d
Xinzhe <38831183+P1Za@users.noreply.github.com>
2025-05-06 14:29:10 -0700

    update

--- hpo_runner.py
+++ hpo_runner.py
@@ -6,24 +6,23 @@ from train import Trainer
 
 def _objective(trial):
     # UNet LoRA
-    r_unet = trial.suggest_int('r_unet', 2, 16)
-    alpha_unet = trial.suggest_int('lora_alpha_unet', 2, 64)
-    dropout_unet = trial.suggest_float('lora_dropout_unet', 0.0, 0.3)
-    lr_unet = trial.suggest_float('lr_unet', 1e-5, 5e-4, log=True)
+    r_unet = trial.suggest_int('r_unet', 4, 16)
+    alpha_unet = trial.suggest_int('lora_alpha_unet', 8, 32)
+    dropout_unet = trial.suggest_float('lora_dropout_unet', 0.0, 0.2)
+    lr_unet = trial.suggest_float('lr_unet', 1e-5, 2e-4, log=True)
     wd_unet = trial.suggest_float('wd_unet', 0.0, 0.3)
 
 
     # Text Encoder LoRA
-    r_text = trial.suggest_int('r_text', 2, 16)
-    alpha_text = trial.suggest_int('lora_alpha_text', 2, 64)
-    dropout_text = trial.suggest_float('lora_dropout_text', 0.0, 0.3)
-    lr_text = trial.suggest_float('lr_text', 1e-6, 2e-4, log=True)
-    wd_text = trial.suggest_float('wd_text', 0.0, 0.2)
+    r_text = trial.suggest_int('r_text', 4, 16)
+    alpha_text = trial.suggest_int('lora_alpha_text', 8, 32)
+    dropout_text = trial.suggest_float('lora_dropout_text', 0.0, 0.2)
+    lr_text = trial.suggest_float('lr_text', 1e-6, 5e-5, log=True)
+    wd_text = trial.suggest_float('wd_text', 0.0, 0.1)
 
     # scheduler
-    lr = trial.suggest_float('lr', 1e-5, 1e-3, log=True)
-    T_max = trial.suggest_int('T_max', 2, 10)
-    eta_min = trial.suggest_float('eta_min', lr * 0.001, lr * 0.1)
+    T_max = trial.suggest_int('T_max', 3, 6)
+    eta_min = trial.suggest_float('eta_min', 1e-6, 1e-4)
 
     batch_size = trial.suggest_categorical('batch_size', [2, 4, 8, 16])
 
@@ -31,7 +30,7 @@ def _objective(trial):
     trainer = Trainer(
         model=model,
         batch_size=batch_size,
-        epochs=10,
+        epochs=2,
         unet_lora_config={
             'r': r_unet,
             'alpha': alpha_unet,
@@ -59,6 +58,7 @@ def _objective(trial):
 def run_hpo(n_trials=30):
     study = optuna.create_study(direction='maximize')
     study.optimize(_objective, n_trials=n_trials)
+    study.optimize(_objective, timeout=1800)
 
     print("Best trial:", study.best_trial)
     return study.best_trial.params
